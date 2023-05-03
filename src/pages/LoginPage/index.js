4b81c6d56e70aa57000444720ca6348b5f24a765
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-04 01:00:07 +0900

    Design: 추가 로그인 페이지

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,25 +1,68 @@
 import React from "react";
 
-import Button from "../../components/Button";
-
 export default function LoginPage() {
     return (
         <div>
-            <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
-                <div className="w-full h-40 p-6 m-3 bg-white rounded shadow lg:w-3/5">
-                    <div className="text-2xl font-medium mt-3">
-                        <div className="text-2xl font-medium mt-3">
-                            로그인페이지!
-                        </div>
-                        <div className="text-sm font-medium mt-3">
-                            최고의 기업. 당신의 미래를 응원합니다.
-                        </div>
+            <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
+                <div className="flex-col items-center w-full h-3/4 p-6 bg-white rounded shadow lg:w-2/5">
+                    <div className="text-2xl font-medium mt-3 text-center">
+                        로그인
                     </div>
-                </div>
-                <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/5">
-                    <div className="flex  mb-2">
-                        <Button value={"로그인"} name={"login"}></Button>
-                    </div>
+                    <form className="flex flex-col">
+                        <div className="grid m-10">
+                            <div>
+                                <label
+                                    for="id"
+                                    class="block text-base font-medium leading-6 text-gray-900"
+                                >
+                                    아이디를 입력하세요
+                                </label>
+                                <div class="mt-2.5 mb-7">
+                                    <input
+                                        type="text"
+                                        name="id"
+                                        id="id"
+                                        autocomplete="given-id"
+                                        class="block w-full rounded-md border-0 px py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label
+                                    for="id"
+                                    class="block text-base font-medium leading-6 text-gray-900"
+                                >
+                                    비밀번호를 입력하세요
+                                </label>
+                                <div class="mt-2.5">
+                                    <input
+                                        type="text"
+                                        name="id"
+                                        id="id"
+                                        autocomplete="given-id"
+                                        class="block w-full rounded-md border-0 px py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="m-10">
+                            <button
+                                type="submit"
+                                class="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                log in
+                            </button>
+                        </div>
+                    </form>
+                    <p class="mt-10 text-center text-sm text-gray-500">
+                        회원이 아니신가요?
+                        <a
+                            href="/join"
+                            class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        >
+                            회원가입하기
+                        </a>
+                    </p>
                 </div>
             </div>
         </div>
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -7,7 +7,7 @@ export default function MainPage() {
     return (
         <div>
             <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
-                <div className="w-full h-40 p-6 m-3 bg-white rounded shadow lg:w-3/5">
+                <div className="w-full h-40 p-6 m-3 bg-white rounded shadow lg:w-2/5">
                     <div className="text-2xl font-medium mt-3">
                         <div className="text-2xl font-medium mt-3">
                             (주) 프람트 솔루션
@@ -17,7 +17,7 @@ export default function MainPage() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/5">
+                <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-2/5">
                     <div className="flex  mb-2">
                         <Button value={"로그인"} name={"login"}></Button>
                         <Button value={"회원가입"} name={"join"}></Button>
