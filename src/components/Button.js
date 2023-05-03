d47c3dc4457426479a9420ab2e88078285dc30a0
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-04 00:13:12 +0900

    Feat: 추가 버튼 클릭시 Navigate

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router";
+import "./Button.css";
 
 export default function Button({ value, name }) {
     const navigate = useNavigate();
 
-    const handleNavi = (name) => {
-        if (name === "login") {
-            navigate("/login");
-        } else {
-            navigate("/join");
-        }
+    const handleNavi = () => {
+        navigate(`/${name}`);
     };
     return (
         <div>
             <button
-                className="rounded-md border p-2 pr-2 mr-3"
+                className="button rounded-md border border-zinc-300 p-2 pr-2 mr-3"
                 onClick={handleNavi}
             >
                 {value}
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+import Button from "../../components/Button";
+
 export default function LoginPage() {
-    return <div></div>;
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-100">
+                <div className="w-full h-40 p-6 m-3 bg-white rounded shadow lg:w-3/5">
+                    <div className="text-2xl font-medium mt-3">
+                        <div className="text-2xl font-medium mt-3">
+                            로그인페이지!
+                        </div>
+                        <div className="text-sm font-medium mt-3">
+                            최고의 기업. 당신의 미래를 응원합니다.
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/5">
+                    <div className="flex  mb-2">
+                        <Button value={"로그인"} name={"login"}></Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
