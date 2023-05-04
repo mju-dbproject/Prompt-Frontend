df7b5ea0aff49d5fd3417463a9bdb11fc394e933
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-04 15:37:09 +0900

    Design: 추가 Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderClosed } from "@fortawesome/free-regular-svg-icons";
+import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
 
 export default function Sidebar() {
@@ -6,5 +10,27 @@ export default function Sidebar() {
         { name: "평가", path: "/evaluation" },
         { name: "마이페이지", path: "/mypage" },
     ];
-    return <div></div>;
+
+    const icons = [
+        { name: faFolderClosed },
+        { name: faChartSimple },
+        { name: faUser },
+    ];
+    // <FontAwesomeIcon icon={faPen} />;
+    return (
+        <div class="w-1/5 h-screen  border-r-2 border-gray-300">
+            {menus.map((name, index) => (
+                <div key={name} class="mb-5 pt-10 px-8">
+                    <button>
+                        <FontAwesomeIcon
+                            icon={icons[index].name}
+                            size="2xl"
+                            style={{ color: "#292929" }}
+                        />
+                        <span class="ml-5">{menus[index].name}</span>
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
 }
