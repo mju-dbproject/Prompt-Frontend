e0e334c65391987983ce66cd3fc59d5364a199db
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-17 00:54:51 +0900

    Refactor: 변경 컴포넌트 분리 ♻️

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -2,6 +2,8 @@ import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input";
+import PwdIcon from "../../components/PwIcon";
 
 export default function LoginPage() {
     const navigate = useNavigate();
@@ -14,16 +16,6 @@ export default function LoginPage() {
 
     const [userId, setUserId] = useState("");
     const [password, setPassword] = useState("");
-
-    const handlePasswordType = (e) => {
-        setPwType(() => {
-            if (!pwType.visible) {
-                return { type: "text", visible: true, icon: faEyeSlash };
-            } else {
-                return { type: "password", visible: false, icon: faEye };
-            }
-        });
-    };
 
     const handleSubmit = () => {
         fetch("http://localhost:3000/user/info", {
@@ -59,12 +51,9 @@ export default function LoginPage() {
                                     아이디를 입력하세요
                                 </label>
                                 <div className="mt-2.5 mb-7">
-                                    <input
+                                    <Input
+                                        label={userId}
                                         type="text"
-                                        name="userid"
-                                        id="userid"
-                                        autocomplete="given-id"
-                                        className="block w-full rounded-md border border-zinc-300 px py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         onChange={(e) =>
                                             setUserId(e.target.value)
                                         }
@@ -79,21 +68,17 @@ export default function LoginPage() {
                                     비밀번호를 입력하세요
                                 </label>
                                 <div className="mt-2.5 relative">
-                                    <input
+                                    <Input
+                                        label={userId}
                                         type={pwType.type}
-                                        name="password"
-                                        id="password"
-                                        autocomplete="given-id"
-                                        className="w-full rounded-md border border-zinc-300 py-2  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         onChange={(e) =>
                                             setPassword(e.target.value)
                                         }
                                     />
 
-                                    <FontAwesomeIcon
-                                        icon={pwType.icon}
-                                        className="absolute top-4 right-3 mb-1.5 text-gray-500"
-                                        onClick={handlePasswordType}
+                                    <PwdIcon
+                                        pwType={pwType}
+                                        setPwType={setPwType}
                                     />
                                 </div>
                             </div>
