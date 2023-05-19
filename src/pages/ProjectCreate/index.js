08945ea755fcfe336c3edfa4792702b9b5b92a9c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-20 02:00:14 +0900

    Design: 변경 일자 입력 제거 💄

--- src/pages/ProjectCreate/index.js
+++ src/pages/ProjectCreate/index.js
@@ -30,8 +30,8 @@ export default function ProjectCreatePage() {
 
             <div className="grid grid-cols-6 mx-auto">
                 <Sidebar className="col-span-1" admin={admin}></Sidebar>
-                <div className="col-span-5 h-screen px-20 pt-10 auto-rows-auto">
-                    <div className="container w-5/6 h-5/6 mx-40 rounded border-2 border-slate-200 px-5 mb-20">
+                <div className="bg-gray-100 col-span-5 h-screen px-20 pt-10 auto-rows-auto">
+                    <div className="bg-white drop-shadow-md container w-5/6 h-5/6 mx-40 rounded border-2 border-slate-200 px-5 mb-20">
                         <div className="text-2xl font-medium pt-10 pb-4 text-start">
                             프로젝트 생성
                         </div>
@@ -86,39 +86,7 @@ export default function ProjectCreatePage() {
                                     ></input>{" "}
                                 </div>
                             </div>
-                            <div className="grid grid-cols-2 gap-2 pt-2 px-0">
-                                <div className="px-2 py-3">
-                                    <label className="text-md font-medium leading-6 text-gray-900 mr-10">
-                                        착수일자
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        id="name"
-                                        autocomplete="given-name"
-                                        className="w-3/5 ml-3 rounded-md border-1 border-zinc-300 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
-                                        onChange={(e) =>
-                                            setStartDate(e.target.value)
-                                        }
-                                    ></input>{" "}
-                                </div>
 
-                                <div className="px-2 py-3">
-                                    <label className="text-md font-medium leading-6 text-gray-900 mr-16">
-                                        종료일자
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        id="name"
-                                        autocomplete="given-name"
-                                        className="w-3/5 rounded-md ml-2.5 border-1 border-zinc-300 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
-                                        onChange={(e) =>
-                                            setEndDate(e.target.value)
-                                        }
-                                    ></input>{" "}
-                                </div>
-                            </div>
                             <div className="grid grid-cols-1 gap-2 pt-3 px-0 mb-2">
                                 <div className="px-2 py-2">
                                     <label className="text-md font-medium leading-6 text-gray-900 mr-16">
