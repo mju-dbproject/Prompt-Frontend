e42b8b72b90f2b4c07522759a7f85142e916d968
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-20 18:36:50 +0900

    Feat: 변경 경로 설정 🚚

--- src/App.js
+++ src/App.js
@@ -9,30 +9,59 @@ import MyPage from "./pages/MyPage";
 import ManagerPage from "./pages/ManagerPage";
 import ProjectCreatePage from "./pages/ProjectCreate";
 import EvaluationPage from "./pages/EvaluationPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
+
+import ProjectManage from "./pages/ProjectManage";
+import EmployeeManage from "./pages/EmployeeManage";
 
 const App = () => {
     return (
         <div className="App">
             <Routes>
+                {/* common */}
                 <Route path="/" element={<MainPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/join" element={<JoinPage />} />
+
+                {/* employee */}
+
                 <Route path="/employee" element={<EmployeePage />} />
+
                 <Route path="/employee/mypage" element={<MyPage />} />
                 <Route
                     path="/employee/evaluation"
                     element={<EvaluationPage />}
                 />
-                <Route path="/manager" element={<ManagerPage />} />
-                <Route path="/manager/mypage" element={<MyPage />} />
+
                 <Route
-                    path="/manager/evaluation"
-                    element={<EvaluationPage />}
+                    path="/employee/detailProject"
+                    element={<ProjectDetailPage />}
+                />
+
+                <Route path="/manager" element={<ManagerPage />} />
+
+                {/* manager */}
+                <Route
+                    path="/manager/employeeList"
+                    element={<EmployeeManage />}
+                />
+                <Route
+                    path="/manager/projectList"
+                    element={<ProjectManage />}
                 />
                 <Route
                     path="/manager/newProject"
                     element={<ProjectCreatePage />}
                 />
+                <Route
+                    path="/manager/detailProject"
+                    element={<ProjectDetailPage />}
+                />
+                <Route
+                    path="/manager/evaluation"
+                    element={<EvaluationPage />}
+                />
+                <Route path="/manager/mypage" element={<MyPage />} />
             </Routes>
         </div>
     );
