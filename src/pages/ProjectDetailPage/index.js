fd14ae5c201747a4af00ed782b9bf1a6f5058572
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-25 18:49:54 +0900

    Test: 추가 특정 프로젝트 상세정보 받아오기 ✅

--- src/pages/ProjectDetailPage/index.js
+++ src/pages/ProjectDetailPage/index.js
@@ -1,20 +1,26 @@
-import React, { useState } from "react";
-import ButtonLists from "../../components/ButtonLists";
+import React, { useState, useEffect } from "react";
+import ButtonLists from "../../components/Atoms/Button/ButtonLists";
 import EmployeeModal from "../../components/EmployeeModal";
-import Header from "../../components/Header";
-import Label from "../../components/Label";
-import ShowTable from "../../components/ShowTable";
+import Header from "../../components/Molecules/Header/Header";
+import Label from "../../components/Atoms/Label/Label";
+import ShowTable from "../../components/Atoms/Table/ShowTable";
 
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../components/Molecules/Sidebar/Sidebar";
 import {
     useRecoilState,
     useRecoilValue,
     useSetRecoilState,
     useResetRecoilState,
 } from "recoil";
+import { useParams } from "react-router";
+import { allProjectsState } from "../../hooks/recoil/atoms";
 
 export default function ProjectDetailPage() {
-    const [isAdmin, setIsAdmin] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(true);
+    const [allProjects, setAllProjects] = useRecoilState(allProjectsState);
+
+    const { id } = useParams();
+    // const detailProject =;
 
     const role = isAdmin ? "경영인" : "직원";
 
@@ -22,6 +28,27 @@ export default function ProjectDetailPage() {
 
     const [isEditing, setIsEditing] = useState(false);
     const [modalOpen, setModalOpen] = useState(false);
+
+    const [detailProject, setDetailProject] = useState({
+        name: "",
+        description: "",
+        client: "",
+        startDate: "",
+        budget: "",
+        manpowerList: [
+            {
+                id: "",
+                name: "",
+                task: "",
+            },
+        ],
+    });
+
+    useEffect(() => {
+        // console.log(id, "is project id!");
+        setDetailProject(allProjects[id - 1]);
+        console.log(detailProject);
+    });
 
     const handleClick = () => {
         setModalOpen(true);
@@ -50,7 +77,7 @@ export default function ProjectDetailPage() {
                                             type="text"
                                             name="description"
                                             id="description"
-                                            className="w-3/5 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                                            className="w-5/6 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
                                         ></input>{" "}
                                     </div>
                                 </div>
@@ -81,7 +108,7 @@ export default function ProjectDetailPage() {
                                             type="text"
                                             name="description"
                                             id="description"
-                                            className="w-3/5 ml-7 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                                            className="w-5/6 ml-7 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
                                         ></input>{" "}
                                     </div>
                                 </div>
@@ -97,7 +124,7 @@ export default function ProjectDetailPage() {
                                             </button>
                                         )}
                                     </div>
-                                    <ShowTable />
+                                    <ShowTable isEditing={isEditing} />
                                 </div>
                             </div>
 
@@ -129,15 +156,14 @@ export default function ProjectDetailPage() {
                     <div className="bg-gray-100 col-span-5 h-screen px-20 pt-10 auto-rows-auto">
                         <div className="bg-white drop-shadow-md container w-5/6 h-5/6 mx-40 rounded border-2 border-slate-200 px-5 mb-20">
                             <div className="text-2xl font-medium pt-10 pb-4 text-start">
-                                사내 웹 서비스 구축
+                                {detailProject.name}
                             </div>
                             <div className="divide-y divide-gray-200">
                                 <div className="grid grid-cols-1 gap-2 pt-4 px-0">
                                     <div className="px-2 py-3">
                                         <Label value="상세 설명" />
                                         <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            사내 근퇴관리를 위한 웹 서비스 구축
-                                            사업
+                                            {detailProject.description}
                                         </span>
                                     </div>
                                 </div>
@@ -146,14 +172,14 @@ export default function ProjectDetailPage() {
                                     <div className="px-2 py-3">
                                         <Label value="발주처" />
                                         <span className="w-5/6 ml-4 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            삼성전자
+                                            {detailProject.client}
                                         </span>
                                     </div>
 
                                     <div className="px-2 py-3">
                                         <Label value="착수일자" />
                                         <span className="w-5/6 ml-4 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            2021.05.01
+                                            {detailProject.startDate}
                                         </span>
                                     </div>
                                 </div>
@@ -161,7 +187,7 @@ export default function ProjectDetailPage() {
                                     <div className="px-2 py-3">
                                         <Label value="예산" />
                                         <span className="w-5/6 ml-7 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0 ml-2">
-                                            5000,000,000 원
+                                            {detailProject.budget}원
                                         </span>
                                     </div>
                                 </div>
@@ -177,7 +203,11 @@ export default function ProjectDetailPage() {
                                             </button>
                                         )}
                                     </div>
-                                    <div className="px-1.5 py-2 w-full h-28 rounded-md border-0.5 border-gray-300 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-main-color sm:text-sm sm:leading-5"></div>
+
+                                    <ShowTable
+                                        isEditing={isEditing}
+                                        detailProject={detailProject}
+                                    />
                                 </div>
                             </div>
                             {isAdmin && (
