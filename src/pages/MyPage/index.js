19844702790ee7c4cd6562891e221a43498f41ab
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-25 18:40:50 +0900

    Test: 추가 유저정보 받아오기 ✅

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../components/Header";
-import Sidebar from "../../components/Sidebar";
+import Header from "../../components/Molecules/Header/Header";
+import Sidebar from "../../components/Molecules/Sidebar/Sidebar";
+import Label from "../../components/Atoms/Label/Label";
 import { useLocation } from "react-router-dom";
-
-import requests from "../../api/requests";
 
 export default function MyPage() {
     const location = useLocation();
@@ -18,22 +17,36 @@ export default function MyPage() {
 
     const [isEditing, setIsEditing] = useState(false);
 
-    const [name, setName] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-    const [email, setEmail] = useState("");
-    const [rank, setRank] = useState("");
-    const [position, setPosition] = useState("");
-    const [skill, setSkill] = useState("");
+    const [userInfo, setUserInfo] = useState({
+        name: "",
+        phoneNumber: "",
+        email: "",
+        rank: "",
+        skill: "",
+    });
+
+    const handleValueChange = (e) => {
+        setUserInfo({
+            ...userInfo,
+            [e.target.name]: e.target.value,
+        });
+    };
 
     useEffect(() => {
         fetchGetInfo();
     }, []);
 
     const fetchGetInfo = async () => {
-        const res = await fetch(requests.fetchUserInfo)
-            .then(console.log("success"))
-            .then((res) => res.json())
-            .then(console.log);
+        const request = await fetch(
+            "https://2d55b3a9-65f0-40be-9a3b-9348ac5d5303.mock.pstmn.io/info"
+        );
+
+        if (request.status === 400 || request.status === 500) {
+            // throw await makeError(response)
+        }
+        const response = await request.json();
+        console.log(response);
+        setUserInfo(response);
     };
 
     const handleSave = () => {
@@ -43,7 +56,7 @@ export default function MyPage() {
     if (isEditing) {
         return (
             <div>
-                <Header role={role}></Header>
+                <Header role={role} isAdmin={isAdmin}></Header>
 
                 <div className="grid grid-cols-6 mx-auto">
                     <Sidebar
@@ -59,68 +72,61 @@ export default function MyPage() {
                             <div className="divide-y divide-gray-200">
                                 <div className="grid grid-cols-2 gap-2 pt-4 px-0">
                                     <div className="px-2 py-3">
-                                        <label className="text-md font-medium leading-6 text-gray-900 mr-10">
-                                            이름
-                                        </label>
+                                        <Label value="이름" />
                                         <input
+                                            value={userInfo.name}
                                             type="text"
                                             name="name"
                                             id="name"
-                                            autocomplete="given-name"
-                                            className="w-3/5 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                                            autoComplete="on"
+                                            className="w-3/5 ml-3.5 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
                                             onChange={(e) =>
-                                                setName(e.target.value)
+                                                handleValueChange(e)
                                             }
                                         ></input>{" "}
                                     </div>
 
                                     <div className="px-2 py-3">
-                                        <label className="text-md font-medium leading-6 text-gray-900 mr-10">
-                                            전화번호
-                                        </label>
+                                        <Label value="전화번호" />
                                         <input
+                                            value={userInfo.phoneNumber}
                                             type="text"
-                                            name="phone-number"
-                                            id="phone-number"
-                                            autocomplete="given-phone-number"
-                                            className="w-3/5 pl-1 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
-                                            onChange={(e) => {
-                                                setPhoneNumber(e.target.value);
-                                            }}
+                                            name="phoneNumber"
+                                            id="phoneNumber"
+                                            autoComplete="on"
+                                            className="w-3/5 ml-3.5 pl-1 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                                            onChange={(e) =>
+                                                handleValueChange(e)
+                                            }
                                         ></input>{" "}
                                     </div>
                                 </div>
                                 <div className="grid grid-cols-1 gap-2 pt-2 px-0">
                                     <div className="px-2 py-3">
-                                        <label className="text-md font-medium leading-6 text-gray-900 mr-7">
-                                            이메일
-                                        </label>
+                                        <Label value="이메일" />
                                         <input
+                                            value={userInfo.email}
                                             type="text"
                                             name="email"
                                             id="email"
-                                            autocomplete="given-email"
+                                            autoComplete="on"
                                             className="w-5/6 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
-                                            onChange={(e) => {
-                                                setEmail(e.target.value);
-                                            }}
+                                            onChange={(e) =>
+                                                handleValueChange(e)
+                                            }
                                         ></input>{" "}
                                     </div>
                                 </div>
                                 <div className="grid grid-cols-2 gap-2 pt-2 px-0">
                                     <div className="px-2 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <div className="text-md font-medium leading-6 text-gray-900">
-                                            직급
-                                        </div>
+                                        <Label value="직급" />
                                         <div className="text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                                             수석
                                         </div>
                                     </div>
 
                                     <div className="px-2 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                                        <div className="text-md font-medium leading-6 text-gray-900">
-                                            포지션
-                                        </div>
+                                        <Label value="포지션" />
                                         <div className="text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
                                             개발자
                                         </div>
@@ -128,19 +134,18 @@ export default function MyPage() {
                                 </div>
                                 <div className="grid grid-cols-1 gap-2 pt-3 px-0">
                                     <div className="px-2 py-2">
-                                        <label className="text-md font-medium leading-6 text-gray-900 mr-10">
-                                            스킬
-                                        </label>
+                                        <Label value="스킬" />
                                         <input
+                                            value={userInfo.skill}
                                             type="text"
                                             name="skill"
                                             id="skill"
-                                            autocomplete="given-skill"
-                                            className="w-5/6 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                                            autoComplete="given-skill"
+                                            className="w-5/6 ml-3.5 rounded-md border-0.5 border-gray-300  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
                                             onChange={(e) =>
-                                                setSkill(e.target.value)
+                                                handleValueChange(e)
                                             }
-                                        ></input>{" "}
+                                        ></input>
                                     </div>
                                 </div>
                             </div>
@@ -148,7 +153,7 @@ export default function MyPage() {
                                 <button
                                     type="submit"
                                     className="flex w-1/5 justify-center rounded-md bg-sub-color px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-main-color focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    onClick={handleSave}
+                                    onClick={() => alert("저장되었습니다!")}
                                 >
                                     저장하기
                                 </button>
@@ -161,7 +166,7 @@ export default function MyPage() {
     } else {
         return (
             <div>
-                <Header role={role}></Header>
+                <Header role={role} isAdmin={isAdmin}></Header>
 
                 <div className="grid grid-cols-6 mx-auto">
                     <Sidebar
@@ -177,59 +182,47 @@ export default function MyPage() {
                             <div className="divide-y divide-gray-200">
                                 <div className="grid grid-cols-2 gap-2 pt-4 px-0">
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-20">
-                                            이름
-                                        </span>
-                                        <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            홍길동
+                                        <Label value="이름" />
+                                        <span className="w-5/6 ml-3.5 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {userInfo.name}
                                         </span>
                                     </div>
 
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-20">
-                                            전화번호
-                                        </span>
+                                        <Label value="전화번호" />
                                         <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            010-1234-5678
+                                            {userInfo.phoneNumber}
                                         </span>
                                     </div>
                                 </div>
                                 <div className="grid grid-cols-1 gap-2 pt-2 px-0">
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-16 pr-1">
-                                            이메일
-                                        </span>
+                                        <Label value="이메일" />
                                         <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            ohoh@gamil.com
+                                            {userInfo.email}
                                         </span>
                                     </div>
                                 </div>
                                 <div className="grid grid-cols-2 gap-2 pt-2 px-0">
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-20">
-                                            직급
-                                        </span>
-                                        <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            수석
+                                        <Label value="직급" />
+                                        <span className="w-5/6 ml-3.5 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {userInfo.rank}
                                         </span>
                                     </div>
 
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-24">
-                                            포지션
-                                        </span>
-                                        <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            개발자
+                                        <Label value="포지션" />
+                                        <span className="w-5/6 ml-3.5 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {userInfo.position}
                                         </span>
                                     </div>
                                 </div>
                                 <div className="grid grid-cols-1 gap-2 pt-3 px-0">
                                     <div className="px-2 py-3">
-                                        <span className="text-md font-medium leading-6 text-gray-900 mr-20">
-                                            스킬
-                                        </span>
-                                        <span className="w-5/6 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                                            python, java
+                                        <Label value="스킬" />
+                                        <span className="w-5/6 ml-4 text-md leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                                            {userInfo.skill}
                                         </span>
                                     </div>
                                 </div>
