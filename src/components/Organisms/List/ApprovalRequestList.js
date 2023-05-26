873a96034c66f18fcfbd3420ed7e124329ee77af
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-05-26 17:06:37 +0900

    Feat: 추가 회원가입 승인/미승인 ✨

--- src/components/Organisms/List/ApprovalRequestList.js
+++ src/components/Organisms/List/ApprovalRequestList.js
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 
 export default function ApprovalRequestList() {
-    const employees = ["홍길동", "힝그리", "양유유", "김여름"];
-    const [approvedEmployees, setApprovedEmployees] = useState([]);
+    const [approvedEmployees, setApprovedEmployees] = useState([
+        {
+            id: "1",
+            name: "홍길동",
+        },
+        {
+            id: "2",
+            name: "힝그리",
+        },
+        {
+            id: "3",
+            name: "양유유",
+        },
+        {
+            id: "4",
+            name: "김여름",
+        },
+    ]);
     const [isHidden, setIshidden] = useState(false);
 
-    const handleApprove = (name) => {
-        const approve = name === "approve";
+    const handleApprove = (emp, name) => {
+        const approve = name === "approve" ? true : false;
         if (approve && window.confirm("승인하시겠습니까?")) {
-            setApprovedEmployees((prevEmployees) => [...prevEmployees, name]);
+            alert("승인하였습니다.");
         } else {
             alert("승인 취소합니다.");
         }
+        handleHideEmployee(emp);
     };
 
-    const handleHideEmployee = (name) => {
-        setApprovedEmployees((prevEmployees) =>
-            prevEmployees.filter((emp) => emp !== name)
+    const handleHideEmployee = (emp) => {
+        let newApprove = approvedEmployees.filter(
+            (employee) => employee.id !== emp
         );
+        console.log("update", newApprove);
+        setApprovedEmployees(newApprove);
     };
 
     return (
@@ -25,37 +44,36 @@ export default function ApprovalRequestList() {
             <section className="bg-white drop-shadow-md items-center place-content-between h-16 mt-3 px-5 border border-slate-200 rounded h-1/2 overflow-y-auto">
                 <div className="divide-y divide-gray-200">
                     <div className="grid grid-cols-1 gap-2 pt-4">
-                        {employees.map((emp, index) => {
-                            if (!approvedEmployees.includes(emp)) {
-                                return (
-                                    <div className="flex" key={index}>
-                                        <div>
-                                            {emp}님이 회원가입 요청을
-                                            보냈습니다.
-                                        </div>
-                                        <div className="justify-end">
-                                            <button
-                                                className="button rounded-md border border-zinc-300 px-2 py-1.5 pr-2 mr-3 my-1"
-                                                onClick={() =>
-                                                    handleApprove({ emp })
-                                                }
-                                            >
-                                                승인
-                                            </button>
-                                            <button
-                                                name="notApprove"
-                                                className="button rounded-md border border-zinc-300 px-2 py-1.5 pr-2 mr-3 my-1"
-                                                onClick={() =>
-                                                    handleHideEmployee({ emp })
-                                                }
-                                            >
-                                                미승인
-                                            </button>
-                                        </div>
+                        {approvedEmployees.map((emp, index) => {
+                            return (
+                                <div className="flex" key={index}>
+                                    <div>
+                                        {emp.name}님이 회원가입 요청을
+                                        보냈습니다.
                                     </div>
-                                );
-                            }
-                            return null; // 승인된 직원은 숨김
+                                    <div className="justify-end">
+                                        <button
+                                            className="button rounded-md border border-zinc-300 px-2 py-1.5 pr-2 mr-3 my-1"
+                                            onClick={() =>
+                                                handleApprove(emp.id, "approve")
+                                            }
+                                        >
+                                            승인
+                                        </button>
+                                        <button
+                                            className="button rounded-md border border-zinc-300 px-2 py-1.5 pr-2 mr-3 my-1"
+                                            onClick={() =>
+                                                handleApprove(
+                                                    emp.id,
+                                                    "notApprove"
+                                                )
+                                            }
+                                        >
+                                            미승인
+                                        </button>
+                                    </div>
+                                </div>
+                            );
                         })}
                     </div>
                 </div>
